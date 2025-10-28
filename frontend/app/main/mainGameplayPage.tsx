3eabcb05785e669b6c2f359efacc105d3515b50c
Chris Marston <chris.scott.marston@gmail.com>
2025-10-28 08:37:05 -0400

    Improved styling and added placeholder fields for all UI elements on main game page.

--- frontend/app/main/mainGameplayPage.tsx
+++ frontend/app/main/mainGameplayPage.tsx
@@ -2,16 +2,23 @@ export function MainGameplayPage() {
 
 	const linkCSS = [
 		"cursor-pointer",
-		"hover:underline",
+		"underline",
+		"decoration-solid",
+		"hover:decoration-dotted",
 		"",
 	].join(" ");
 
 	return (
-		<main className="flex flex-col items-center h-screen p-4 bg-stone-800 text-lg text-stone-100">
+		<main className="flex flex-col items-center h-screen p-4 bg-stone-700 text-lg text-wheat">
 			<div className="flex gap-2 h-auto">
 				<div className={linkCSS}>Inventory</div>
 				<div className={linkCSS}>Equipment</div>
 			</div>
+			<div>~</div>
+			<div id="locationHeader">LOCATION HEADER</div>
+			<div id="contentArea">CONTENT AREA</div>
+			<div id="notificationsBar">NOTIFICATIONS BAR</div>
+			<div id="controlsBar">CONTROLS BAR</div>
 		</main>
 	);
 }
--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -10,6 +10,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
+// TODO: What are all of these?
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
